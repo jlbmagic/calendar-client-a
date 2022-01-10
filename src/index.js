aab568b590ac3d6dfab3d9fb0ede8b25db509f32
Jeremy Brown <jlbmagic@gmail.com>
2022-01-10 15:17:44 -0500

    initial set up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 //The first function. Remove this.
 const btn = document.querySelector("button");
 btn.onclick = function () {
-  alert("You ran some JavaScript");
+  alert("You ran some JavaScript!!!!");
 };
 
 //
